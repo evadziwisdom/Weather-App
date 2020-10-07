08d824fb5c9146b4357053f8a52ba63c8b3bc185
evadzwisdom <evadziwisdom@gmail.com>
2020-10-07 12:11:47 +0000

    evadziwisdom

---
+++ src/BackgroundComponent.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./App.css"
+
+function BackgroundComponent () {
+    return (
+        <>
+        <img src="weatherBackground.jpg" alt="backgroundimage" className="bg" />
+        </>
+
+    );
+
+}
+
+export default BackgroundComponent
